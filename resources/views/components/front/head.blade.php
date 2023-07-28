7754e3f0f1e3bb3d3ae918c781f1fd0ca941370a
Riad <youlo13371488@gmail.com>
2023-07-28 13:06:38 +0400

    Landing page's header

---
+++ resources/views/components/front/head.blade.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ 'assets/front/css/components/header.css' }}">
+  <link rel="stylesheet" href="{{ 'assets/front/css/common/reset.css' }}">
+  <link rel="stylesheet" href="{{ 'assets/front/css/common/main.css' }}">
+  <title>CBS</title>
+</head>
