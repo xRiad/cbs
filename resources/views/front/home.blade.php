dfde81ed6b2450fad0832aed0cae99299dffb81c
Riad <youlo13371488@gmail.com>
2023-07-28 15:35:35 +0400

    Landing pagge / course

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,1 +1,15 @@
 @extends('front.layouts.app')
+
+@section('links')
+  <link rel="stylesheet" href="{{ 'assets/front/css/home.css' }}">
+@endsection
+
+@section('content')
+   <div class="services">
+        <h4 class="services-head">Xidmətlərimiz</h4>
+        <div class="services-text-btn">
+            <div>CBS olaraq strateji həllər və yaradıcı fikirlərin bir arada olduğu rəqəmsal xitmətlərimizi təqdim edirik.</div>
+            <div class="btn1">Bİzİmlə əlaqə</div>
+        </div>
+   </div> 
+@endsection
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <x-front.head></x-front.head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CBS</title>
+  <link rel="stylesheet" href="{{ 'assets/front/css/components/header.css' }}">
+  <link rel="stylesheet" href="{{ 'assets/front/css/common/reset.css' }}">
+  <link rel="stylesheet" href="{{ 'assets/front/css/common/main.css' }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  @yield('links')
+</head>
 <body>
   <x-front.header></x-front.header>
+  @yield('content')
 </body>
 </html>
